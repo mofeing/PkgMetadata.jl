71fcff033e059658c32297a11ba40aa3f3b6fac6
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2023-01-15 17:55:35 +0100

    Comment `cmap`,`cfilter`

--- src/PkgMetadata.jl
+++ src/PkgMetadata.jl
@@ -10,7 +10,10 @@ using Base.Filesystem: joinpath
 
 projectfiles() = map(dep -> joinpath(dep.second.source, "Project.toml"), Pkg.API.dependencies())
 
+# curry map
 cmap(f) = Fix1(map, f)
+
+# curry filter
 cfilter(f) = Fix1(filter, f)
 
 metadata() = projectfiles() |> cmap(parsefile) |> cfilter(info -> haskey(info, "metadata")) |> cmap() do info
