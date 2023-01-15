fbaa08a2ebb6893081eb27f92b1f3486a363f285
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2023-01-15 13:53:33 +0100

    Init repository

---
+++ src/PkgMetadata.jl
@@ -0,0 +1,20 @@
+baremodule PkgMetadata
+
+export metadata
+
+using Pkg
+using TOML: parsefile
+using Base.Iterators: map, filter
+using Base: Fix1, haskey, |>, =>, Dict
+using Base.Filesystem: joinpath
+
+projectfiles() = map(dep -> joinpath(dep.second.source, "Project.toml"), Pkg.API.dependencies())
+
+cmap(f) = Fix1(map, f)
+cfilter(f) = Fix1(filter, f)
+
+metadata() = projectfiles() |> cmap(parsefile) |> cfilter(info -> haskey(info, "metadata")) |> cmap() do info
+                 info["name"] => info["metadata"]
+             end |> Dict
+
+end
